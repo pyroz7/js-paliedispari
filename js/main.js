269b35e6a977f7f2c7d7a09677daeb9b6aa3c54c
pyroz7 <vincipyroz7@gmail.com>
2023-11-20 18:58:03 +0100

    Half

--- js/main.js
+++ js/main.js
@@ -17,4 +17,23 @@ function reverseParola(str) {
     let strReverse = str.split("").reverse().join("");
     return strReverse;
 }
+
+
+//CHIEDERE ALL'UTENTE DI SCEGLIERE PARI O DISPARI E UN NUMERO DA 1 A 5
+
+let pariDispari = prompt("Pari o Dispari");
+let scegliNumero = prompt("Scegli un numero da 1 a 5");
+
+//NUMERO RANDOM PER PC
+
+let numberPc = Math.random(1,5);
+let number = console.log(numberPc);
+
+//SOMMIAMO I NUMERI
+
+let sumNumber = (scegliNumero + numberPc) ;
+
+//CREIAMO UNA FUNZIONE PER COMPLETARE 
+
+
  
