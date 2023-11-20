59d709ad9fcd2bbb59d833581528f3e9a880938d
pyroz7 <vincipyroz7@gmail.com>
2023-11-20 16:35:13 +0100

    half (?)

---
+++ js/main.js
@@ -0,0 +1,29 @@
+//CHIEDERE ALL'UTENTE DI INSERIRE UNA PAROLA
+
+let word = prompt("parola"); 
+ 
+//CREARE UNA FUNZIONE PER VERIFICARE SE LA PAROLA è PALINDROMA
+
+function palindrome () {
+      let resulte = parola.reverse();
+
+      if (parola == resulte) {
+        return parola + "è palindroma";
+      }
+
+      else 
+      return parola + "non è palindroma"
+
+
+
+
+
+
+
+
+
+
+
+}
+
+ 
